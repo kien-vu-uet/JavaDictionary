39212b4496265f1f0973552bb5d7eedc0accdb72
Wizard <76658438+StahlDev@users.noreply.github.com>
2021-10-19 00:24:27 +0700

    Update ReadText.java

--- src/SourceItems/ReadText.java
+++ src/SourceItems/ReadText.java
@@ -6,8 +6,7 @@ public class ReadText {
 
 	public static void speech(String text) {
 		VoiceManager voiceManager = VoiceManager.getInstance();
-		// gọi đến giọng của thằng có tên là kevin 16, có thằng khác tên là kevin và một thằng tên
-		// là anna hay gì gì đó, tôi đọc trên tuts mà quên mất rồi ??
+		// gọi đến giọng của thằng có tên là kevin 16, có thằng khác tên là kevin và một thằng tên alan
 		com.sun.speech.freetts.Voice syntheticVoice = voiceManager.getVoice("kevin16");
 		syntheticVoice.allocate();
 		syntheticVoice.speak(text);
